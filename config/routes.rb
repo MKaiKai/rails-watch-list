15eb1950a9246187e94993fd881538c92e32bc7b
Mariama KaiKai <mmak.webdev@gmail.com>
2023-01-19 21:13:43 +0900

    added card and images

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 root to: "lists#index"
 
-resources :lists, only: [:index, :new, :show, :create] do
+resources :lists, only: [:new, :show, :create] do
     resources :bookmarks, only: [:new, :show, :create, :destroy]
 end
   # Defines the root path route ("/")
